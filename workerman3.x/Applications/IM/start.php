fe4d8cb0a6943f73192374a5618e25a9aba6050a
qmo <55141448@qq.com>
2015-03-12 23:50:59 +0800

    win7

--- workerman3.x/Applications/IM/start.php
+++ workerman3.x/Applications/IM/start.php
@@ -2,14 +2,14 @@
 	// 引入Workerman命名空间
 	use Workerman\Worker;
 	// 初始化一个worker
-	$httpWorker = new Worker("websocket://0.0.0.0:2558");
+	$httpWorker = new Worker("websocket://0.0.0.0:8383");
 	// 设置进程数量
 	$httpWorker->count = 4;
 
 	// 接收到浏览器数据时原路返回
 	$httpWorker->onMessage = function($connection,$data){
 		
-		$connection->send('888888888888888');
+		$connection->send($data);		
 
 	};
 
