6607341d68611e90ae0abfe6929cecc568eff7bc
qmo <55141448@qq.com>
2015-03-12 21:33:12 +0800

    win7

---
+++ workerman3.x/Applications/IM/start.php
@@ -0,0 +1,20 @@
+<?php
+	// 引入Workerman命名空间
+	use Workerman\WebServer
+	// 初始化一个worker
+	$httpWorker = new Worker("http://0.0.0.0:2558");
+	// 设置进程数量
+	$httpWorker->count = 4;
+
+	// 接收到浏览器数据时原路返回
+	$httpWorker->onMessage = function($connection,$data){
+		$connection->send(var_dump($connection));
+		$connection->send($data);
+	}
+
+	
+
+
+
+
+
