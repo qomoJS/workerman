53522058b44b034a92f52a3653a0bd5d5abe935e
qmo <55141448@qq.com>
2015-03-17 22:18:22 +0800

    win7

--- workerman3.x/Applications/IM/start.php
+++ workerman3.x/Applications/IM/start.php
@@ -1,31 +1,42 @@
 <?php
 	// 引入Workerman命名空间
-	use Workerman\Worker;
-	// 初始化一个worker
-	$httpWorker = new Worker("websocket://0.0.0.0:2558");
-	// 设置进程数量
-	$httpWorker->count = 4;
+	// use Workerman\Worker;
+	// // 初始化一个worker
+	// $httpWorker = new Worker("websocket://0.0.0.0:2558");
+	// // 设置进程数量
+	// $httpWorker->count = 4;
 
-	// 接收到浏览器数据时原路返回
-	$httpWorker->onMessage = function($connection,$data){
-		$return = "";
-		switch ($data) {
-			case '你好':
-				$return = "hello,小可爱";
-				break;
-			case '吃饭了没?':
-				$return = "还没，你请我吃吧！";
-				break;
-			default:
-				$return = "呵呵，你说什么鸟语呢？";
-				break;
-		}
+	// // 接收到浏览器数据时原路返回
+	// $httpWorker->onMessage = function($connection,$data){
+	// 	$return = "";
+	// 	switch ($data) {
+	// 		case '你好':
+	// 			$return = "hello,小可爱";
+	// 			break;
+	// 		case '吃饭了没?':
+	// 			$return = "还没，你请我吃吧！";
+	// 			break;
+	// 		default:
+	// 			$return = "呵呵，你说什么鸟语呢？";
+	// 			break;
+	// 	}
+	// 	$connection->send($return);
+	// };
 
 
-		$connection->send($return);
+	use \GatewayWorker\Gateway;
 
+	// 指定websocket协议
+	$gateway = new Gateway("websocket://0.0.0.0:2558");
+
+	$gateway->onMessage = function ($clientId,$message){
+
+		$gateway->sendToCurrentClient($clientId.'----'.$message);
 
 	};
+
+
+
 
 
 
